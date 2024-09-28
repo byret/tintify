d5dbe740f2be08bbe625e3fb2872584aab8309f5
byret <byrett@gmail.com>
2024-09-28 18:47:22 +0200

    configuring for heroku

--- src/main/java/com/pixapp/pixapp/config/WebConfig.java
+++ src/main/java/com/pixapp/pixapp/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.pixapp.pixapp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +13,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/avatars/**")
                 .addResourceLocations("file:avatars/");
+    }
+    @Bean
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
     }
 }
